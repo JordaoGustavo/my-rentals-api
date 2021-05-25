7502da26c18da8d40ae41ee28127923b92733ee8
JordaoGustavo <jordaogustavo3@gmail.com>
2021-05-24 22:07:32 -0300

    inicia projeto em branco, adiciona camadas de dominio e aplicacao, adiciona gitignore e algumas entidades

---
+++ MyRentals.Api.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MyRentals.Api.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
