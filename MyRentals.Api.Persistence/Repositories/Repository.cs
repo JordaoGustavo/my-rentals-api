3085525f5a064976b2db75cad607f5cc2286acc8
JordaoGustavo <jordaogustavo3@gmail.com>
2021-05-26 20:10:36 -0300

    adiciona camanda de persistencia

---
+++ MyRentals.Api.Persistence/Repositories/Repository.cs
@@ -0,0 +1,35 @@
+﻿using MyRentals.Api.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyRentals.Api.Persistence.Repositories
+{
+    public abstract class Repository<T> : IAsyncRepository<T> where T : class
+    {
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync<TId>(TId id) where TId : struct
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
