294fef52076377d35fe130888a5cfc684a79c77b
JordaoGustavo <jordaogustavo3@gmail.com>
2021-05-26 20:21:46 -0300

    adiciona dependency injection para a camada de persistence

--- MyRentals.Api.Persistence/Repositories/Repository.cs
+++ MyRentals.Api.Persistence/Repositories/Repository.cs
@@ -1,33 +1,39 @@
 ﻿using MyRentals.Api.Application.Contracts.Persistence;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace MyRentals.Api.Persistence.Repositories
 {
     public abstract class Repository<T> : IAsyncRepository<T> where T : class
     {
-        public Task<T> AddAsync(T entity)
+        public async Task<IDbConnection> GetConnectionAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteAsync(T entity)
+        public async Task<T> AddAsync(T entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> GetByIdAsync<TId>(TId id) where TId : struct
+        public async Task DeleteAsync(T entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IReadOnlyList<T>> ListAllAsync()
+        public async Task<T> GetByIdAsync<TId>(TId id) where TId : struct
         {
             throw new NotImplementedException();
         }
 
-        public Task UpdateAsync(T entity)
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(T entity)
         {
             throw new NotImplementedException();
         }
